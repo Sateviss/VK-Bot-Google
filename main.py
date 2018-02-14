e3529e366f51a47b1fd4b729871ec6959e6db7d2
Eugene Pisarchick <sateviss@gmail.com>
2018-02-15 00:09:42 +0300

    = send_message

--- main.py
+++ main.py
@@ -32,18 +32,22 @@ def get_message(c_id):
 
 def send_message(c_id, text):
     delay = 0.01
+    if c_id > 1000:
+        if get_user(c_id)['can_write_private_message'] == 0:
+            return "err"
     while 1:
         try:
+            m = 0
             if len(text) < 4000:
-                if c_id>100000:
+                if c_id > 1000:
                     m = api.messages.send(user_id=c_id, message=text)
                 else:
                     m = api.messages.send(chat_id=c_id, message=text)
             else:
-                if c_id>100000:
-                    m = api.messages.send(user_id=c_id, message="Очень длинное сообщение, которое начинается на "+text[:100])
+                if c_id > 1000:
+                    m = api.messages.send(user_id=c_id, message="Очень длинное сообщение, которое начинается на "+text[:200])
                 else:
-                    m = api.messages.send(chat_id=c_id, message="Очень длинное сообщение, которое начинается на "+text[:100])
+                    m = api.messages.send(chat_id=c_id, message="Очень длинное сообщение, которое начинается на "+text[:200])
             return m
         except:
             time.sleep(delay)
@@ -71,7 +75,17 @@ def get_user(u_id=None):
         while 1:
             try:
                 if u_id is not None:
-                    u = api.users.get(user_id=u_id)[0]
+                    u = api.users.get(user_id=u_id, fields="photo_id, verified, sex, bdate, city, country, home_town, "
+                                                           "has_photo, photo_50, photo_100, photo_200_orig, photo_200, "
+                                                           "photo_400_orig, photo_max, photo_max_orig, online, domain, "
+                                                           "has_mobile, contacts, site, education, universities, schools, "
+                                                           "status, last_seen, followers_count, common_count, occupation, "
+                                                           "nickname, relatives, relation, personal, connections, exports, "
+                                                           "wall_comments, activities, interests, music, movies, tv, books,"
+                                                           " games, about, quotes, can_post, can_see_all_posts, can_see_audio,"
+                                                           " can_write_private_message, can_send_friend_request, is_favorite,"
+                                                           " is_hidden_from_feed, timezone, screen_name, maiden_name, crop_photo,"
+                                                           " is_friend, friend_status, career, military, blacklisted, blacklisted_by_me")[0]
                 else:
                     u = api.users.get()[0]
                 user_dict.update({u_id:u})
