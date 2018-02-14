65064c636352a8202955560e28d2c4b131a9e7b8
Unknown <sateviss@gmail.com>
2018-02-14 09:44:56 +0300

    Init

---
+++ main.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+
+import vk
+import time
+import sys
+from datetime import datetime
+
+file = open("login.txt", "r")
+login = file.readline()
+password = file.readline()
+file.close()
+
+session = vk.AuthSession(app_id=6322567, user_login=login, user_password=password, scope=268435455)
+api = vk.API(session)
+ID = 1
+
+
+def get_message(c_id):
+    delay = 0.01
+    while 1:
+        try:
+            mess = api.messages.getHistory(chat_id=c_id, count=1)[1]
+            return mess
+        except:
+            time.sleep(delay)
+            delay *= 2
+            continue
+
+
+def send_message(c_id, text):
+    delay = 0.01
+    while 1:
+        try:
+            m = api.messages.send(chat_id=c_id, message=text)
+            return m
+        except:
+            time.sleep(delay)
+            delay *= 2
+            continue
+
+
+def delete_message(m_id):
+    delay = 0.01
+    while 1:
+        try:
+            api.messages.delete(message_ids=m_id, delete_for_all=1)
+            return
+        except:
+            time.sleep(delay)
+            delay *= 2
+            continue
+
+
+def get_user(u_id=None):
+    delay = 0.01
+    while 1:
+        try:
+            if u_id is not None:
+                u = api.users.get(user_id=u_id)[0]
+                return u
+            else:
+                u = api.users.get()[0]
+                return u
+        except:
+            time.sleep(delay)
+            delay *= 2
+            continue
+
+
+def gen_message(n):
+    o = ''
+    for i in range(n - 2):
+        i += 2
+        o += str(i) + ": "
+        k = []
+        x = 2
+        while i != 1:
+            if i % x == 0:
+                i /= x
+                k.append(x)
+                x = 1
+            x += 1
+        o += str(k) + "\n"
+    return o
+
+
+print("...working")
+send_message(ID, "---Бот Запущен---")
+last_message = -1
+nahui = []
+me = get_user()['uid']
+while 1:
+    time.sleep(0.2)
+    mess = get_message(ID)
+
+    i = 0
+    while i < len(nahui):
+        n = nahui[i]
+        if n[1] < time.time():
+            delete_message(n[0])
+            nahui.pop(i)
+            continue
+        n[1] -= 0.2
+        i += 1
+
+    if last_message != mess['mid']:
+        if mess['uid'] == 136776175 and mess['body'] == u"/стоп":
+            send_message(ID, "--ок, ухажу--")
+            sys.exit(0)
+        if mess['uid'] != me:
+            last_message = mess['mid']
+            user = get_user(mess['uid'])
+            print(time.strftime("%d.%m.%y - %H:%M:%S "), user['first_name'], user['last_name']+" :"+mess['body'])
+            if mess['uid'] == 445077792:
+                nahui.append([send_message(ID, "Юра, иди нахуй"), time.time() + 10])
+                print(time.strftime("%d.%m.%y - %H:%M:%S ", time.localtime()), "Юра нахуй")
+            if (mess['uid'] == 461460001 or mess['uid'] == 463718240 or mess['uid'] == 465167934) and mess['body'] == '':
+                send_message(ID, gen_message(49))
+                send_message(ID, "АНТИПОРН")
+                print(time.strftime("%d.%m.%y - %H:%M:%S ", time.localtime()), "Антипорн!")
+
