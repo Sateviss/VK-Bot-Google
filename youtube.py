d4240013730842d5992ce321083d4ce1b236d6d5
Sateviss <sateviss@gmail.com>
2018-02-16 12:40:23 +0300

    YT module update added file size limit

--- youtube.py
+++ youtube.py
@@ -7,8 +7,10 @@ import requests
 
 def down_and_send(v_id, ID):
     try:
-        subprocess.run("youtube-dl --write-info-json --geo-bypass -o \"Download/%(id)s/video.%(ext)s\" "
-                       "-f mp4 {}".format(quote(v_id)), shell=1)
+        out = b''
+        subprocess.run("youtube-dl --write-info-json --geo-bypass --max-filesize 300m "
+                       "-o \"Download/%(id)s/video.%(ext)s\" "
+                       "-f mp4 {}".format(quote(v_id)), shell=1, stdout=out)
         js = json.load(open("Download/"+v_id+"/video.info.json"))
         resp = wrap.get_video_link(js['title'], js['description'])
         f = open("Download/"+v_id+"/video.mp4", 'rb')
@@ -18,5 +20,22 @@ def down_and_send(v_id, ID):
         subprocess.run("rm -rf Download/{}".format(quote(v_id)), shell=1)
         return "--done--"
     except:
+        if out.find(b'File is larger than max-filesize'):
+            try:
+                wrap.send_message(ID, "--Лучшее качество занимает слишком много места, переход на худшее--")
+                subprocess.run("youtube-dl --write-info-json --geo-bypass --max-filesize 300m "
+                               "-o \"Download/%(id)s/video.%(ext)s\" "
+                               "-f worst,mp4 {}".format(quote(v_id)), shell=1, stdout=out)
+                js = json.load(open("Download/" + v_id + "/video.info.json"))
+                resp = wrap.get_video_link(js['title'], js['description'])
+                f = open("Download/" + v_id + "/video.mp4", 'rb')
+                r = requests.post(url=resp['upload_url']
+                                  , files={'file': f})
+                wrap.send_attachment(ID, "", "video470444116_" + str(r.json()['video_id']))
+                subprocess.run("rm -rf Download/{}".format(quote(v_id)), shell=1)
+                return "--done--"
+            except:
+                subprocess.run("rm -rf Download/{}".format(quote(v_id)), shell=1)
+                return "--error--"
         subprocess.run("rm -rf Download/{}".format(quote(v_id)), shell=1)
         return "--error--"
