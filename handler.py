dce00817af094898e8803005e1cdf8dbcac645f1
sateviss <sateviss@gmail.com>
2018-02-25 10:28:59 +0300

    array index fix

--- handler.py
+++ handler.py
@@ -121,7 +121,7 @@ class Handler:
                     self.bot.send_message(ID, "--Добавлено {0} сообщений--".format(s))
                 if isint(mess['body'].split()[1]):
                     n = int(mess['body'].split()[1])
-                    if n < abs(len(self.quote_lines)):
+                    if abs(n) < len(self.quote_lines):
                         self.bot.send_message(ID, self.quote_lines[n])
                     else:
                         self.bot.send_message(ID, "--Столько цитат ещё не добавлено, пока что их {0}--".format(len(self.quote_lines)))
