33a3368c898511559aa0bc5cfdff0cb18617d9ed
Unknown <sateviss@gmail.com>
2018-03-03 10:41:14 +0300

    One more try

--- youtube.py
+++ youtube.py
@@ -11,9 +11,8 @@ def down_and_send(v_id, chat_id, bot: VkWrap):
     try:
         bot.send_message(chat_id, "--Запрос принят, ожидайте--")
         out = subprocess.run(
-            "youtube-dl --write-info-json --geo-bypass --max-filesize 300m "
-            "-o \"Download/%(id)s/video.%(ext)s\" "
-            "-f \"worst/mp4\" {}".format(quote(v_id)),
+            args=['youtube-dl', '--write-info-json', '--geo-bypass', '--max-filesize 300m',
+                  '-o "Download/%(id)s/video.%(ext)s"', '-f mp4 {0}'.format(quote(v_id))],
             shell=True,
             stdout=subprocess.PIPE
         ).stdout
@@ -30,16 +29,14 @@ def down_and_send(v_id, chat_id, bot: VkWrap):
             try:
                 subprocess.run("rm -rf Download/{}".format(quote(v_id)), shell=True)
                 bot.send_message(chat_id, "--Лучшее качество занимает слишком много места, переход на худшее--")
-                subprocess.run(
-                    "youtube-dl --write-info-json --geo-bypass --max-filesize 300m "
-                    "-o \"Download/%(id)s/video.%(ext)s\" "
-                    "-f \"worst/mp4\" {}".format(quote(v_id)),
-                    shell=True
-                )
+                subprocess.run("youtube-dl --write-info-json --geo-bypass --max-filesize 300m "
+                               "-o \"Download/%(id)s/video.%(ext)s\" "
+                               "-f \"worst/mp4\" {}".format(quote(v_id)), shell=True)
                 js = json.load(open("Download/" + v_id + "/video.info.json"))
                 resp = bot.get_video_link(js['title'], js['description'])
                 f = open("Download/" + v_id + "/video.mp4", 'rb')
-                r = requests.post(url=resp['upload_url'], files={'file': f})
+                r = requests.post(url=resp['upload_url']
+                                  , files={'file': f})
                 bot.send_attachment(chat_id, "", "video{0}_{1}".format(r.json()['owner_id'], r.json()['id']))
                 subprocess.run("rm -rf Download/{}".format(quote(v_id)), shell=True)
                 return "done"
